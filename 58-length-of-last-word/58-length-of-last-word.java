794cff8eb9bfd7f547440a9404bfbfcb07150903
Obi Nnaduruaku <70237266+obi9999n@users.noreply.github.com>
2022-01-23 20:16:41 -0500

    Time: 0 ms (100.00%), Space: 38.8 MB (44.26%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        if (s.length() == 1) return 1;
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        int answer = 0;
+        int j = s.length() - 1;
+        
+        while(j > -1 && s.charAt(j) == ' '){
+            j--;
+        }
+        
+        if (j == -1) return 0;
+        
+        while(j > -1 && s.charAt(j) != ' '){
+            answer++;
+            j--;
+        }
+        
+        return answer;
+    }
+}
