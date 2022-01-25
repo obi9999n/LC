97a0ef136821e327dd6e669d9fab69b2e0ce5447
Obi Nnaduruaku <70237266+obi9999n@users.noreply.github.com>
2022-01-25 14:56:46 -0500

    Time: 957 ms (5.00%), Space: 568 MB (5.00%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,30 +1,31 @@
 class Solution {
     public boolean isPalindrome(String s) {
         if (s.length() == 1) return true;
-        String alpha = "abcdefghijklmnopqrstuvwxyz0123456789";
-        String s2 = "";
-        for (char ch: s.toLowerCase().toCharArray()){
-            if (alpha.indexOf(ch) > -1){
-                s2 = s2 + Character.toString(ch);
+        s = s.toLowerCase();
+        String alphabet = "abcdefghijklmnopqrstuvwxyz0123456789";
+        ArrayList<Character> list = new ArrayList<Character>();
+        for(char ch: s.toCharArray()){
+            if (alphabet.contains(Character.toString(ch))){
+                list.add(ch);
             }
         }
-        String s3 = s2;
-        StringBuilder sb = new StringBuilder();
-        sb.append(s2);
+        
+        if (list.size() == 0) return true;
+        String string = "";
+        for(int i = 0; i < list.size(); i++){
+            string = string + Character.toString(list.get(i));
+        }
+        
         
         int i = 0;
-        int j = s2.length() - 1;
+        int j = string.length() - 1;
+        
         while(i < j){
-            char temp = sb.charAt(i);
-            sb.setCharAt(i, sb.charAt(j)); 
-            sb.setCharAt(j, temp);
+            if(string.charAt(i) != string.charAt(j)) return false;
             i++;
             j--;
         }
         
-        s2 = sb.toString();
-        
-        return s2.equals(s3);
-        
+        return true;
     }
 }
