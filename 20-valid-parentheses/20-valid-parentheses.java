c478a14a394974459d48805641faf98570c386e5
Obi Nnaduruaku <70237266+obi9999n@users.noreply.github.com>
2022-02-05 09:04:44 -0500

    Time: 3 ms (50.95%), Space: 42.4 MB (5.17%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,23 +1,34 @@
 class Solution {
     public boolean isValid(String s) {
-        if (s.length() == 1) return false;
-        
-        HashMap<Character, Character> map = new HashMap<Character, Character>();
-        map.put('(',')');
-        map.put('[',']');
-        map.put('{','}');
-        Stack<Character> stack = new Stack<Character>();
-        
-        for(char ch: s.toCharArray()){
-            if(map.containsKey(ch)){
-                stack.push(ch);  
-            } else if (stack.isEmpty() || ch != map.get(stack.peek())) {
-                return false;
-            } else {
-                stack.pop();
-            }  
+        if (s.length() == 1){
+            return false;
         }
-                
-        return stack.isEmpty();    
+        //edge case
+        
+        HashMap<Character,Character> map = new HashMap<Character,Character>();
+        Stack<Character> stack = new Stack<Character>();
+        map.put('(', ')');
+        map.put('[', ']');
+        map.put('{', '}');
+        
+        for (int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            char top;
+            if (map.containsKey(c)){
+                stack.push(c);
+            } else if (stack.isEmpty()){
+                    return false;   
+            } else {
+                top = stack.peek();
+                if (map.get(top) == c){
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        
+        return stack.isEmpty();
+        
     }
 }
