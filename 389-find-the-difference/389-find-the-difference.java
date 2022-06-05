3ba77107962183788888f249df88a7ed9b01e7f9
Obi Nnaduruaku <70237266+obi9999n@users.noreply.github.com>
2022-06-05 16:09:03 -0400

    Time: 12 ms (14.12%), Space: 42.3 MB (51.54%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.java
+++ 389-find-the-difference/389-find-the-difference.java
@@ -1,28 +1,27 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        char character = '0';
         
-        //add all characters of string s to map
-        for (char c: s.toCharArray()){
-            map.put(c, map.getOrDefault(c, 0) + 1);
+        if(s.length() == 0){
+            return t.charAt(0);
+        }
+        HashMap<Character,Integer> map = new HashMap<>();
+        
+        for(char c: s.toCharArray()){
+            map.put(c,map.getOrDefault(c, 0) + 1);
         }
         
-        for(char x: t.toCharArray()){
-            if (map.containsKey(x)){
-                if (map.get(x) == 1){
-                    map.remove(x);    
-                } else {
-                    map.put(x, map.get(x) - 1);
-                }
-            } else{
-                character = x;
-                break;
+        for (int i = 0; i < t.length(); i++){
+            char x = t.charAt(i);
+            if (map.containsKey(x) && map.get(x) == 1){
+                map.remove(x);
+            } else if(map.containsKey(x) && map.get(x) > 1) {
+                map.put(x, map.get(x) - 1);    
+            } else {
+                return x;
             }
         }
         
-        return character;
         
-        
+        return 'z';
     }
 }
