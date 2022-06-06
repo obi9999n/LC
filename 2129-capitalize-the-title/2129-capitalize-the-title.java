d241d01fdd5c48848a02e47e4cb33f1b2423a561
Obi Nnaduruaku <70237266+obi9999n@users.noreply.github.com>
2022-06-06 19:42:29 -0400

    Time: 6 ms (60.00%), Space: 43.1 MB (51.56%) - LeetHub

--- 2129-capitalize-the-title/2129-capitalize-the-title.java
+++ 2129-capitalize-the-title/2129-capitalize-the-title.java
@@ -1,50 +1,33 @@
 class Solution {
     public String capitalizeTitle(String title) {
-        title = title.toLowerCase();
-        
-        //make whole string lowercase
-        if (title.length() == 1) return title;
-        
-        //edge case
-        String newString = "";
-        int L = title.length();
-        
+        char [] arr = title.toCharArray();
         int i = 0;
-        int j = i;
+        int j = 0;
+        String answer = "";
+        int currLen = 0;
         
-        while(j < L){
-            while(j != L && title.charAt(j) != ' '){
+        while (j < arr.length){
+            if(arr[j] != ' '){
+                currLen++;
+                arr[j] = Character.toLowerCase(arr[j]);
                 j++;
+            } else {
+                if (currLen > 2){
+                    arr[i] = Character.toUpperCase(arr[i]);
+                }
+                currLen = 0;
+                i = j + 1;
+                j = i;
             }
-            if (j == L){
-                break;
-            }
-            StringBuilder sb = new StringBuilder();
-            sb.append(title.substring(i, j));
-            int len = sb.length();
-            if (len > 2){
-                sb.setCharAt(0, Character.toUpperCase(sb.charAt(0)));
-            }
-            newString = newString + sb.toString() + " ";
-            i = j + 1;
-            j++;
+        }
+        System.out.println(arr[i]);
+        System.out.println(currLen);
+
+        //last word in arr
+        if (currLen > 2){
+            arr[i] = Character.toUpperCase(arr[i]);
         }
         
-        //last word
-            StringBuilder sb1 = new StringBuilder();
-            sb1.append(title.substring(i, j));
-            int len = sb1.length();
-            if (len > 2){
-                sb1.setCharAt(0, Character.toUpperCase(sb1.charAt(0)));
-            }
-        newString = newString + sb1.toString();
-        
-        return newString;
-        
-        
-        
-        
-        
-        
+        return String.valueOf(arr);
     }
 }
