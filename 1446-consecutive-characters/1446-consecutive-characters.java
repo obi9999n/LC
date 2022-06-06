dbe36f06c8ac04c92e15bd412dbf5df0c0e675ad
Obi Nnaduruaku <70237266+obi9999n@users.noreply.github.com>
2022-06-06 19:57:39 -0400

    Time: 8 ms (6.26%), Space: 44.9 MB (5.99%) - LeetHub

---
+++ 1446-consecutive-characters/1446-consecutive-characters.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int maxPower(String s) {
+        //edge case
+        if(s.length() == 1){
+            return 1;
+        }
+        //minimum is 1 no mater what
+        int maxPow = 1;
+        int currPow = 1;
+        //hashmap to track unique char & two pointer solution
+        HashMap<Character, Integer> map = new HashMap<>();
+        int i = 0;
+        int j = 1;
+        map.put(s.charAt(0), 1);
+        
+        //loop through string
+        while (j < s.length()){
+            if(map.containsKey(s.charAt(j))){
+                currPow++;
+            } else {
+                maxPow = Math.max(maxPow, currPow);
+                map.clear();
+                i = j;
+                map.put(s.charAt(i), 1);
+                currPow = 1;
+            }
+            
+            maxPow = Math.max(maxPow, currPow);
+            j++;
+            
+        }
+        
+        return maxPow;
+    }
+}
