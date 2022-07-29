1f64bf9357a9b61d9947d8eb6bffba705e70fa6c
Obi Nnaduruaku <70237266+obi9999n@users.noreply.github.com>
2022-07-29 18:28:19 -0400

    Time: 0 ms (100.00%), Space: 42.2 MB (18.64%) - LeetHub

--- 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.java
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.java
@@ -10,27 +10,31 @@
  */
 class Solution {
     public int getDecimalValue(ListNode head) {
-        double length = 0.0;
-        double value = 0;
-        String binaryNum = "";
-            
+        //reverse linked list
         ListNode curr = head;
-        while(curr != null){
-            binaryNum = binaryNum + Integer.toString(curr.val);
+        ListNode prev = null;
+        
+        while(curr != null) {
+            ListNode nn = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nn;
+        }
+        curr = prev;
+        
+        //for each 1, do 2^x  X: 0,1,2,3...
+        double power = 0.0;
+        double ans = 0.0;
+        while (curr != null){
+            if (curr.val == 1){
+                ans = ans + Math.pow(2.0, power);
+            }
+            power = power + 1.0;
             curr = curr.next;
         }
         
-        int i = binaryNum.length() - 1;
-        while(i >= 0){
-            if (binaryNum.charAt(i) == '1'){
-                value = value + Math.pow(2.0, length);
-            }
-            length = length + 1.0;
-            i--;
-        }
-        
-        int trueValue = (int)value;
-        return trueValue;
+        return (int) ans ;
         
     }
+    
 }
