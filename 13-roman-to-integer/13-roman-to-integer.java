d1c819bdbf59ad39258816b5a6f62cb3a3f7299c
Obi Nnaduruaku <70237266+obi9999n@users.noreply.github.com>
2022-08-30 13:26:14 -0400

    Time: 16 ms (18.58%), Space: 48.9 MB (12.21%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int romanToInt(String s) {
+        int answer = 0;
+        
+        //map | char:int
+        Map<Character, Integer> map = new HashMap<Character,Integer>();
+        map.put('M', 1000);
+        map.put('D', 500);
+        map.put('C', 100);
+        map.put('L', 50);
+        map.put('X', 10);
+        map.put('V', 5);
+        map.put('I', 1);
+        
+        //special instances map| str:int
+        Map<String, Integer> sMap = new HashMap<String,Integer>();
+        sMap.put("IV", 4);
+        sMap.put("IX", 9);
+        sMap.put("XL", 40);
+        sMap.put("XC", 90);
+        sMap.put("CD", 400);
+        sMap.put("CM", 900);
+        
+        //loop through string
+        int i = 0;
+        while(i < s.length() - 1){
+            if(sMap.containsKey(s.substring(i, i + 2))){
+                answer = answer + sMap.get(s.substring(i, i + 2));
+                i += 2;
+            } else {
+                answer = answer + map.get(s.charAt(i));
+                i++;
+            }
+        }
+        
+        if (i < s.length()){
+            answer = answer + map.get(s.charAt(i));
+        }
+        
+        return answer;
+    }
+}
