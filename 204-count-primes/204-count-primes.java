85800da5fa84138dd88a02f4fe86c3cd581f64fb
Obi Nnaduruaku <70237266+obi9999n@users.noreply.github.com>
2022-09-02 14:25:35 -0400

    Time: 195 ms (46.57%), Space: 69.1 MB (14.89%) - LeetHub

---
+++ 204-count-primes/204-count-primes.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int countPrimes(int n) {
+        //edge case
+        if(n <= 2){
+            return 0;
+        }
+        
+        boolean[] arr = new boolean[n];
+        
+        for(int i = 2; i <= (int) Math.sqrt(n); ++i){
+            if(arr[i] == false){
+                for(int j = i*i; j < n; j += i){
+                    arr[j] = true;
+                }
+            }
+        }
+        
+        int ans = 0;
+        for (int z = 2; z < arr.length; z++){
+            if (arr[z] == false) ans++;
+        }
+        
+        return ans;
+    }
+}
